5e2e7635cabd8539f5ae5f48586e630c62e6be59
Lee <d.lee@cashoff.ru>
2021-04-14 13:46:59 +0300

    Удалить остаточный secret key

--- tracker/settings.py
+++ tracker/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ufp*jy0k#-nmhaj$wi*@2x1+8)b)^ad^4f6(g!uv_qom!5pih2'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
